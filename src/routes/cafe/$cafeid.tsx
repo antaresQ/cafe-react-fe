a6c223fbe88c1dc11927c9a78c0d6d17beefa07d
Qhairil Mundzir <antares_elementium@hotmail.com>
2024-09-30 17:05:28 +0800

    added hook result

--- src/routes/cafe/$cafeid.tsx
+++ src/routes/cafe/$cafeid.tsx
@@ -36,116 +36,123 @@ export default function UpdateCafe() {
   var isValidCafeUUID = uuid.validate(cafeid)
 
   const cafeQ = getCafe(cafeid);
+
+  if(cafeQ.isLoading){
+    return <div>Loading Cafe details...</div>
+  }
   
-  const onFill = () => {
-    form.setFieldsValue({
-      name: cafeQ.data.name,
-      description: cafeQ.data.description,
-      logo: cafeQ.data.logo,
-      location: cafeQ.data.location
-    })
-  }
-
-  const toCafesPage = () =>{
-    return navigate({to:'/cafes/$location', params:{location: 'null'}});
-  }
-
-  const onFinish = (values: Cafe) => {
-    console.log(values)
-
-    if(isUpsertError){
-      return toast.error(upsertError.message)
-    }
-    else {
-      toast.success(isValidCafeUUID ? `Cafe Updated: ${cafeQ.data.name}` : 'Cafe Added')
-    }
-
-    return toCafesPage();
-  }
-
-  const onReset = () => {
-    form.resetFields()
-  }
-
-  const onSubmit = async () => {
-
-    await form.validateFields()
-      .catch((err) => {
-        let errors = form.getFieldsError().filter(e => e.errors.length > 0)
-        return console.log(errors);
+  if(cafeQ.data)
+  {
+    const onFill = () => {
+      form.setFieldsValue({
+        name: cafeQ.data.name,
+        description: cafeQ.data.description,
+        logo: cafeQ.data.logo,
+        location: cafeQ.data.location
       })
-    
-    let cafeData = form.getFieldsValue();
-    if(isValidCafeUUID && cafeQ.data.id.toLowerCase() == cafeid.toLowerCase()){
-      cafeData.id = cafeid.toUpperCase();
     }
-    
-    await upsertCafe(cafeData);
-  }
 
-  if (cafeQ.data) {
-    onFill()
-  }
+    const toCafesPage = () =>{
+      return navigate({to:'/cafes/$location', params:{location: 'null'}});
+    }
 
-  return (
-    <div>
-      <div>{isValidCafeUUID ? 'Edit' : 'Add'} Cafe</div>
-      {isValidCafeUUID ? <div>Cafe Id: {cafeid}</div> : null}
-      <Form
-        {...layout}
-        form={form}
-        name="control-hooks"
-        onFinish={onFinish}
-        style={{ maxWidth: 600 }}
-      >
-        <Form.Item
-          name="name"
-          label="Name"
-          rules={[{ required: true }, { min: 6 }, { max: 10 }]}
+    const onFinish = (values: Cafe) => {
+      console.log(values)
+
+      if(isUpsertError){
+        return toast.error(upsertError.message)
+      }
+      else {
+        toast.success(isValidCafeUUID ? `Cafe Updated: ${cafeQ.data.name}` : 'Cafe Added')
+      }
+
+      return toCafesPage();
+    }
+
+    const onReset = () => {
+      form.resetFields()
+    }
+
+    const onSubmit = async () => {
+
+      await form.validateFields()
+        .catch((err) => {
+          let errors = form.getFieldsError().filter(e => e.errors.length > 0)
+          return console.log(errors);
+        })
+      
+      let cafeData = form.getFieldsValue();
+      if(isValidCafeUUID && cafeQ.data.id.toLowerCase() == cafeid.toLowerCase()){
+        cafeData.id = cafeid.toUpperCase();
+      }
+      
+      await upsertCafe(cafeData);
+    }
+
+    if (cafeQ.data) {
+      onFill()
+    }
+
+    return (
+      <div>
+        <div>{isValidCafeUUID ? 'Edit' : 'Add'} Cafe</div>
+        {isValidCafeUUID ? <div>Cafe Id: {cafeid}</div> : null}
+        <Form
+          {...layout}
+          form={form}
+          name="control-hooks"
+          onFinish={onFinish}
+          style={{ maxWidth: 600 }}
         >
-          <Input />
-        </Form.Item>
-       
-        <Form.Item
-          name="description"
-          label="Description"
-          rules={[{ required: true }, { max: 256 }]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          name="logo"
-          label="Logo"
-          rules={[{ required: false }]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          name="location"
-          label="Location"
-          rules={[{ required: true }]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item {...tailLayout}>
-          <Space>
-            <Button type="primary" htmlType="submit"
-              onClick={onSubmit}
-            >
-              Submit
-            </Button>
-            <Button
-              htmlType="button"
-              onClick={cafeQ.data ? onFill : onReset}
-            >
-              Reset
-            </Button>
-            <Button type="primary" htmlType="button" onClick={toCafesPage} danger>
-              Cancel
-            </Button> 
-          </Space>
-        </Form.Item>
-      </Form>
-    </div>
-  )
+          <Form.Item
+            name="name"
+            label="Name"
+            rules={[{ required: true }, { min: 6 }, { max: 10 }]}
+          >
+            <Input />
+          </Form.Item>
+        
+          <Form.Item
+            name="description"
+            label="Description"
+            rules={[{ required: true }, { max: 256 }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="logo"
+            label="Logo"
+            rules={[{ required: false }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="location"
+            label="Location"
+            rules={[{ required: true }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item {...tailLayout}>
+            <Space>
+              <Button type="primary" htmlType="submit"
+                onClick={onSubmit}
+              >
+                Submit
+              </Button>
+              <Button
+                htmlType="button"
+                onClick={cafeQ.data ? onFill : onReset}
+              >
+                Reset
+              </Button>
+              <Button type="primary" htmlType="button" onClick={toCafesPage} danger>
+                Cancel
+              </Button> 
+            </Space>
+          </Form.Item>
+        </Form>
+      </div>
+    )
+  }
 }
