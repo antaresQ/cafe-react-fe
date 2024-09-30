3e4c3bf952dfe4a470de18cacda00718eb27714c
Qhairil Mundzir <antares_elementium@hotmail.com>
2024-09-30 10:25:00 +0800

    vite config un

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,12 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 export default defineConfig({
   plugins: [TanStackRouterVite({}), react()],
   server: {
-    // proxy: {
-    //   '/api': 'http://localhost:8060'
-    // }
     proxy: {
-      '/api': 'http://192.168.11.191:8060'
+      '/api': 'http://localhost:8060'
     },
+    // proxy: {
+    //   '/api': 'http://192.168.11.191:8060'
+    // },
     host: true,
     port: 3000
   }
