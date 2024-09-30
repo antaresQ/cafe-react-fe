d3ea8a4312f8ab33e647e427e32201f4d802b8b9
Qhairil Mundzir <antares_elementium@hotmail.com>
2024-09-30 17:02:44 +0800

    fix employee edit page issues

--- src/routes/employee/$employeeid.tsx
+++ src/routes/employee/$employeeid.tsx
@@ -33,150 +33,166 @@ export default function EmployeeEdit() {
   const { employeeid } = Route.useParams()
   const isEmployeeId = employeeid.substring(0, 2).toLowerCase() === 'ui'
   const [form] = Form.useForm()
+  const allValues = Form.useWatch([], form);
   
   const cafes = getCafes()
   const employeeQ = getEmployee(employeeid)
   const { mutate:upsertEmployee, isError:isUpsertError, error:upsertError } = useEmployeeData();
   const navigate = useNavigate()
 
-  const onFill = () => {
-    form.setFieldsValue({
-      name: employeeQ.data.name,
-      gender: employeeQ.data.gender,
-      email_Address: employeeQ.data.email_Address,
-      phone_Number: employeeQ.data.phone_Number,
-      cafe_Id: cafes.data.filter((cafe:Cafe) => cafe.name == employeeQ.data?.cafe)[0]?.id,
-      start_Date: dayjs(employeeQ.data.start_Date?.slice(0, 10)),
-    })
+  
+
+  // if (employeeQ.data?.id.length > 0) {
+  //   onFill()
+  // }
+
+  if(employeeQ.isLoading || cafes.isLoading) {
+    return <div>Loading.....</div>
   }
 
-  const toEmployeesPage = () =>{
-
-    return navigate({to:'/employees/$cafeId', params:{cafeId: 'null'}});
-  }
-
-  const onFinish = (values: EmployeeCreateUpdate) => {
-    console.log(values)
-
-    if(isUpsertError){
-      return toast.error(upsertError.message)
-    }
-    else {
-      toast.success(isEmployeeId ? `Employee Updated: ${employeeQ.data.name}` : 'Employee Added')
-    }
-
-    return toEmployeesPage();
-  }
-
-  const onReset = () => {
-    form.resetFields()
-  }
-
-  const onSubmit = async () => {
-
-    await form.validateFields()
-      .catch((err) => {
-        let errors = form.getFieldsError().filter(e => e.errors.length > 0)
-        return console.log(errors);
+  if(employeeQ.data && cafes.data)
+  {
+    const onFill = () => {
+      form.setFieldsValue({
+        name: employeeQ.data.name,
+        gender: employeeQ.data.gender,
+        email_Address: employeeQ.data.email_Address,
+        phone_Number: employeeQ.data.phone_Number,
+        cafe_Id: cafes.data.filter((cafe:Cafe) => cafe.name == employeeQ.data?.cafe)[0]?.id,
+        start_Date: employeeQ.data ? dayjs(employeeQ.data.start_Date?.slice(0, 10)) : dayjs(new Date().toISOString().slice(0, 10)),
       })
-    
-    let employeeData = form.getFieldsValue();
-    if (isEmployeeId && employeeQ.data.id.toLowerCase() == employeeid.toLowerCase()){
-      employeeData.id = employeeid.toUpperCase();
+    }
+  
+    const toEmployeesPage = () =>{
+  
+      return navigate({to:'/employees/$cafeId', params:{cafeId: 'null'}});
+    }
+  
+    const onFinish = (values: EmployeeCreateUpdate) => {
+      console.log(values)
+  
+      if(isUpsertError){
+        return toast.error(upsertError.message)
+      }
+      else {
+        toast.success(isEmployeeId ? `Employee Updated: ${employeeQ.data.name}` : 'Employee Added')
+      }
+  
+      return toEmployeesPage();
+    }
+  
+    const onReset = () => {
+      form.resetFields()
+    }
+  
+    const onSubmit = async () => {
+  
+      await form.validateFields()
+        .catch((err) => {
+          let errors = form.getFieldsError().filter(e => e.errors.length > 0)
+          return console.log(errors);
+        })
+      
+      let employeeData = form.getFieldsValue();
+      if (isEmployeeId && employeeQ.data.id.toLowerCase() == employeeid.toLowerCase()){
+        employeeData.id = employeeid.toUpperCase();
+      }
+  
+      employeeData.phone_Number = parseInt(employeeData.phone_Number);
+      employeeData.start_Date = new Date(employeeData.start_Date).toISOString()
+      
+      await upsertEmployee(employeeData);
+  
     }
 
-    employeeData.phone_Number = parseInt(employeeData.phone_Number);
-    employeeData.start_Date = new Date(employeeData.start_Date).toISOString()
-    
-    await upsertEmployee(employeeData);
-
-  }
-
-  if (employeeQ.data?.id.length > 0) {
-    onFill()
-  }
-
-  return (
-    <div>
-      <div>{isEmployeeId ? 'Edit' : 'Add'} Employee</div>
-      {isEmployeeId ? <div>UserId: {employeeid}</div> : null}
-      <Form
-        {...layout}
-        form={form}
-        name="control-hooks"
-        onFinish={onFinish}
-        style={{ maxWidth: 600 }}
-      >
-        <Form.Item
-          name="name"
-          label="Name"
-          rules={[{ required: true }, { min: 6 }, { max: 10 }]}
+    return (
+      <div>
+        <div>{isEmployeeId ? 'Edit' : 'Add'} Employee</div>
+        {isEmployeeId ? <div>UserId: {employeeid}</div> : null}
+        <Form
+          {...layout}
+          form={form}
+          name="control-hooks"
+          onFinish={onFinish}
+          style={{ maxWidth: 600 }}
         >
-          <Input />
-        </Form.Item>
-        <Form.Item name="gender" label="Gender" rules={[{ required: true }]}>
-          <Radio.Group
-            //placeholder="Select a option and change input text above"
-            //onChange={onGenderChange}
-            //allowClear
+          <Form.Item
+            name="name"
+            label="Name"
+            rules={[{ required: true }, { min: 6 }, { max: 10 }]}
+            initialValue={employeeQ?.data?.name}
           >
-            <Radio.Button value="Male">Male</Radio.Button>
-            <Radio.Button value="Female">Female</Radio.Button>
-          </Radio.Group>
-        </Form.Item>
-        <Form.Item
-          name="email_Address"
-          label="Email Address"
-          rules={[{ required: true }, { type: 'email' }]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          name="phone_Number"
-          label="Phone Number"
-          rules={[{ required: true }, { min: 8, max: 8 }]}
-        >
-          <Input type='text'/>
-        </Form.Item>
-        <Form.Item name="cafe_Id" label="Cafe" rules={[{ required: true }]}>
-          <Select
-            placeholder="Select a option and change input text above"
-            //onChange={onCafeChange}
-            allowClear
+            <Input />
+          </Form.Item>
+          <Form.Item name="gender" label="Gender" rules={[{ required: true }]} initialValue={employeeQ?.data?.gender}>
+            <Radio.Group
+              //placeholder="Select a option and change input text above"
+              //onChange={onGenderChange}
+              //allowClear
+            >
+              <Radio.Button value="Male">Male</Radio.Button>
+              <Radio.Button value="Female">Female</Radio.Button>
+            </Radio.Group>
+          </Form.Item>
+          <Form.Item
+            name="email_Address"
+            label="Email Address"
+            rules={[{ required: true }, { type: 'email' }]}
+            initialValue={employeeQ?.data?.email_Address}
           >
-            {cafes.data?.map((cafe: Cafe) => {
-              return parse(
-                `<Option value="${cafe.id}">${cafe.name}</Option>`,
-              )
-            })}
-          </Select>
-        </Form.Item>
-        <Form.Item
-          name="start_Date"
-          label="Start Date"
-          rules={[{ required: true }]}
-        >
-          <DatePicker />
-        </Form.Item>
-        <Form.Item {...tailLayout}>
-          <Space>
-            <Button type="primary" htmlType="submit"
-              onClick={onSubmit}
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="phone_Number"
+            label="Phone Number"
+            rules={[{ required: true }, { min: 8, max: 8 }]}
+            initialValue={employeeQ?.data?.phone_Number}
+          >
+            <Input type='text'/>
+          </Form.Item>
+          <Form.Item name="cafe_Id" label="Cafe" rules={[{ required: true }]} initialValue={employeeQ?.data?.cafe}>
+            <Select
+              placeholder="Select a option and change input text above"
+              //onChange={onCafeChange}
+              allowClear
             >
-              Submit
-            </Button>
-            <Button
-              htmlType="button"
-              onClick={employeeQ.data ? onFill : onReset}
-            >
-              Reset
-            </Button>
-            <Button type="primary" htmlType="button" onClick={toEmployeesPage} danger>
-              Cancel
-            </Button>
-          </Space>
-        </Form.Item>
-      </Form>
-    </div>
-  )
+              {cafes.data?.map((cafe: Cafe) => {
+
+                return <Select.Option key={cafe.id} value={cafe.id}>{cafe.name}</Select.Option>
+                // return parse
+                //   `<Select.Option key="${cafe.id}" value="${cafe.id}">${cafe.name}</Select.Option>`,
+                // )
+              })}
+            </Select>
+          </Form.Item>
+          <Form.Item
+            name="start_Date"
+            label="Start Date"
+            rules={[{ required: true }]}
+            initialValue={dayjs(employeeQ?.data?.start_Date.slice(0, 10))}
+          >
+            <DatePicker />
+          </Form.Item>
+          <Form.Item {...tailLayout}>
+            <Space>
+              <Button type="primary" htmlType="submit"
+                onClick={onSubmit}
+              >
+                Submit
+              </Button>
+              <Button
+                htmlType="button"
+                onClick={employeeQ.data ? onFill : onReset}
+              >
+                Reset
+              </Button>
+              <Button type="primary" htmlType="button" onClick={toEmployeesPage} danger>
+                Cancel
+              </Button>
+            </Space>
+          </Form.Item>
+        </Form>
+      </div>
+    )
+  }
 }
